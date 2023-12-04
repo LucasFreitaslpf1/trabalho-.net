49696a40312380a45a349c84f92e8ac1b4d72c3e
lpf1 <lpf1@aluno.ifnmg.edu.br>
2023-12-04 00:04:09 -0300

    migration postagem

---
+++ Migrations/20231204030249_Postagem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace trabalho.net.Migrations
+{
+    /// <inheritdoc />
+    public partial class Postagem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "postagem",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    datahora = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    texto = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_postagem", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "postagem",
+                schema: "public");
+        }
+    }
+}
